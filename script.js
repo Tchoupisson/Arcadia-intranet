5ef7df16b6f598e218d093a42cdeda2fa6a0faa3
Tchoupisson <tchoupisson56@gmail.com>
2024-07-12 14:50:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Your Firebase configuration
+script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyDL1u1v8MaAIsEdi_nJOtGvbAjSfeharbs",
     authDomain: "arcadia-intranet.firebaseapp.com",
